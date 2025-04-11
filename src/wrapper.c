e4bd5b9be9ffa8bbf3b32b290056afc8157d4056
Koen Hufkens <koen.hufkens@gmail.com>
2025-04-11 12:22:36 +0200

    updating the model file to igrf v14

--- src/wrapper.c
+++ src/wrapper.c
@@ -5,7 +5,7 @@
 #include <R_ext/Rdynload.h>
 
 // Fortran code
-void F77_NAME(igrf13)(
+void F77_NAME(igrf14)(
     int *isv,
     double *date,
     int *itype,
@@ -16,7 +16,7 @@ void F77_NAME(igrf13)(
   );
 
 // C wrapper
-extern SEXP c_igrf13_f(
+extern SEXP c_igrf14_f(
     SEXP isv,
     SEXP date,
     SEXP itype,
@@ -34,7 +34,7 @@ extern SEXP c_igrf13_f(
   SEXP out = PROTECT( allocMatrix(REALSXP, 1, 4) );
 
   // Fortran call
-  F77_CALL(igrf13)(
+  F77_CALL(igrf14)(
      INTEGER(isv),
      REAL(date),
      INTEGER(itype),
@@ -57,12 +57,12 @@ extern SEXP c_igrf13_f(
 }
 
 static const R_CallMethodDef CallEntries[] = {
-  {"c_igrf13_f",   (DL_FUNC) &c_igrf13_f,   6},
+  {"c_igrf14_f",   (DL_FUNC) &c_igrf14_f,   6},
   {NULL,         NULL,                0}
 };
 
 void R_init_swiftr(DllInfo *dll) {
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
-  R_RegisterCCallable("igrf", "c_igrf13_f",  (DL_FUNC) &c_igrf13_f);
+  R_RegisterCCallable("igrf", "c_igrf14_f",  (DL_FUNC) &c_igrf14_f);
 }
